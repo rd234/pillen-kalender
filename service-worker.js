b5e68fe2babd8fa5326ba55a8107ae48b281eb5b
rd234 <45292754+rd234@users.noreply.github.com>
2025-07-10 22:22:23 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'pillenk-cache-v1';
+const urlsToCache = [
+  './PillenK_20250706_413db8.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
